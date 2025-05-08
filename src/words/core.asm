01c181aae75458f5a27a9ac08742e5cee42d2718
Sergei Igolnikov <paramendula@yandex.ru>
2025-05-09 01:36:45 +0300

    key and emit fixed; + some ITC procedures

--- src/words/core.asm
+++ src/words/core.asm
@@ -12,6 +12,24 @@ section .data
 
 cfa_exit dq STUB_EXIT
 cfa_branch dq BRANCH
+cfa_literal dq LITERAL
+cfa_jump dq JUMP
+
+word_eq:
+  dq 0
+  db 0
+  db 1
+  db "="
+  align 8
+  word_eq_cfa dq word_eq_exec
+
+word_dup:
+  dq 0
+  db 0
+  db 3
+  db "DUP"
+  align 8
+  word_dup_cfa dq word_dup_exec
 
 word_emit:
   dq 0
@@ -37,14 +55,42 @@ word_quit:
   align 8
   word_quit_cfa dq STUB
   ; THREADED CODE
+.loop:
   dq word_key_cfa
-  dq word_key_cfa
+  dq word_dup_cfa
+  dq cfa_literal
+  dq 'Q'
+  dq word_eq_cfa
+  dq cfa_branch
+  dq .if_false
   dq word_emit_cfa
-  dq word_emit_cfa
+  dq cfa_jump
+  dq .loop
+.if_false:
   dq cfa_exit
 
 
 section .text
+
+word_dup_exec:
+  mov rax, QWORD [rsp]
+  push rax
+
+  EXIT
+
+word_eq_exec:
+  pop rax
+  pop rdx
+  cmp rax, rdx
+  je .true
+
+  push 0
+  EXIT
+.true:
+  push -1
+  EXIT
+
+; ITC handlers
 
 STUB:
   NEXT
@@ -58,10 +104,38 @@ STUB_EXIT:
 
 ; ( DATA: f -- ) ( r10: 8+ADDR-IF-FALSE -- )
 BRANCH:
+  mov r10, QWORD [rbp]
+
   pop rax
-  jz .false
-  NEXT 
-.false:
-  mov r10, [r10+8]
-  sub r10, 8
+  cmp rax, 0
+
+  jne .true
+
+  add r10, 16
+  mov QWORD [rbp], r10
+  mov r10, QWORD [r10]
+  jmp [r10]
+.true:
+  add r10, 8
+  mov r10, QWORD [r10]
+  mov QWORD [rbp], r10 
+  mov r10, QWORD [r10]
+  jmp [r10]
+
+; ( r10: 8+JUMP-ADDRESS )
+JUMP:
+  mov r10, QWORD [rbp]
+  mov r10, QWORD [r10+8]
+  mov QWORD [rbp], r10
+  mov r10, QWORD [r10]
+  jmp [r10]
+
+; (DATA : -- x ) ( r10: 8+VALUE -- )
+LITERAL:
+  mov r10, QWORD [rbp]
+  add rbp, 8
+  add r10, 8
+  mov rax, QWORD [r10]
+  push rax
+
   NEXT
