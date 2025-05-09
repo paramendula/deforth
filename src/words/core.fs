4c08fd4327f2bfcfd296225eb0181d0ca9ebb463
Sergei Igolnikov <paramendula@yandex.ru>
2025-05-10 02:17:56 +0300

    words/core* added for future plans

---
+++ src/words/core.fs
@@ -0,0 +1,14 @@
+: QUIT
+  ( empty the return stack and set the input source to the user input device )
+  POSTPONE [
+   REFILL
+  WHILE
+   ['] INTERPRET CATCH
+   CASE
+   0 OF STATE @ 0= IF ." OK" THEN CR ENDOF
+   -1 OF ( TODO: Aborted ) ENDOF
+   -2 OF ( TODO: Display message from ABORT" ) ENDOF
+   ( default ) DUP ."Exception #" .
+   ENDCASE
+  REPEAT BYE
+;
